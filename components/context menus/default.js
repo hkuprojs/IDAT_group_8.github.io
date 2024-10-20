7bf3441f6a760707aee54502a19253ddc169124e
boris sigma <1322553126@qq.com>
2024-10-20 23:00:12 +0800

    updated

--- components/context menus/default.js
+++ components/context menus/default.js
@@ -10,9 +10,6 @@ function DefaultMenu(props) {
                 <span className="ml-5">❗</span> <span className="ml-2">Report bugs</span>
             </a>
             <Devider />
-            <a rel="noreferrer noopener" href="https://www.zhihu.com/people/peter-95-27-1" target="_blank" className="w-full block cursor-default py-0.5 hover:bg-ub-warm-grey hover:bg-opacity-20 mb-1.5">
-                <span className="ml-5">🙋‍♂️</span> <span className="ml-2">Follow on <strong>Zhihu</strong></span>
-            </a>
             <a rel="noreferrer noopener" href="https://github.com/Group8-Jobs" target="_blank" className="w-full block cursor-default py-0.5 hover:bg-ub-warm-grey hover:bg-opacity-20 mb-1.5">
                 <span className="ml-5">🤝</span> <span className="ml-2">Follow on <strong>Github</strong></span>
             </a>
--- components/screen/booting_screen.js
+++ components/screen/booting_screen.js
@@ -11,11 +11,6 @@ function BootingScreen(props) {
                     : <img width="40px" height="40px" className={" w-10 " + (props.visible ? " animate-spin " : "")} src="./themes/Yaru/status/process-working-symbolic.svg" alt="Ubuntu Process Symbol" />)}
             </div>
             <img width="200px" height="100px" className="md:w-1/5 w-1/2" src="./themes/Yaru/status/ubuntu_white_hex.svg" alt="Ubuntu Name" />
-            <div className="text-white mb-4">
-                <a className="underline" href="https://www.zhihu.com/people/peter-95-27-1" rel="noreferrer noopener" target="_blank">zhihu</a>
-                <span className="font-bold mx-1"> | </span>
-                <a href="https://github.com/Group8-Jobs" rel="noreferrer noopener" target="_blank" className="underline">github</a>
-            </div>
         </div>
     )
 }
